b33d124fe9aaabd638f17fec4fbf5969fbc9dc88
zerotohero200222 <aebharath17@gmail.com>
2025-11-10 19:04:29 +0530

    Add Terraform provider configuration for Google Cloud

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
