f1c983990125b9cc6425978b16d6b4ed5d9b9dc8
zerotohero200222 <aebharath17@gmail.com>
2026-02-12 00:22:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
-output "pubsub_topic_name" {
-  description = "The created Pub/Sub topic"
-  value       = google_pubsub_topic.topic.name
+output "topic_name" {
+  value = google_pubsub_topic.topic.name
 }
 
-output "pubsub_subscription_name" {
-  description = "The created Pub/Sub subscription"
-  value       = google_pubsub_subscription.subscription.name
+output "subscription_name" {
+  value = google_pubsub_subscription.subscription.name
 }
+
