2670d9fe8db990982eb93342958f2be59d642efb
zerotohero200222 <aebharath17@gmail.com>
2026-02-12 00:21:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,45 @@
+########################################
 # Pub/Sub Topic
+########################################
 resource "google_pubsub_topic" "topic" {
   name = "${var.topic_name}-${var.environment}"
+
+  message_retention_duration = "604800s" # 7 days
+
+  labels = {
+    environment = var.environment
+  }
 }
 
-# Pub/Sub Subscription
+
+########################################
+# Push Subscription
+########################################
 resource "google_pubsub_subscription" "subscription" {
   name  = "${var.subscription_name}-${var.environment}"
   topic = google_pubsub_topic.topic.name
 
-  ack_deadline_seconds = 20
-  message_retention_duration = "86400s" # 1 day
+  ack_deadline_seconds = var.ack_deadline_seconds
+
+  push_config {
+    push_endpoint = var.push_endpoint
+
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+
+  expiration_policy {
+    ttl = ""
+  }
+
+  labels = {
+    environment = var.environment
+  }
 }
+
