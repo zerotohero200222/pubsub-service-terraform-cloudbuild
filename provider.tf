73ef929ffd011a315e41dd99475e7c464ace5f68
zerotohero200222 <aebharath17@gmail.com>
2026-02-12 00:21:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
 }
+
